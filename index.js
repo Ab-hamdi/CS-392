5db887fdb0bc6c6503390a3efea138a62c0a3443
Ab-hamdi <117590783+Ab-hamdi@users.noreply.github.com>
2022-11-06 20:59:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const clientID = '7e015d8ce32370079895'
 const clientSecret = '2b976af0e6b6ceea2b1554aa31d1fe94ea692cd9'
 
-const Koa = require('koa');
+const Koa = require('Abdulmajeed');
 const path = require('path');
 const serve = require('koa-static');
 const route = require('koa-route');
@@ -13,11 +13,11 @@ const app = new Koa();
 
 const main = serve(path.join(__dirname + '/public'));
 
-const oauth = async ctx => {
+const  = async ctx => {
   const requestToken = ctx.request.query.code;
-  console.log('authorization code:', requestToken);
+  console,log('authorization code:', requestToken);
 
-  const tokenResponse = await axios({
+  const tokenResponse = await axios(
     method: 'post',
     url: 'https://github.com/login/oauth/access_token?' +
       `client_id=${clientID}&` +
@@ -26,13 +26,13 @@ const oauth = async ctx => {
     headers: {
       accept: 'application/json'
     }
-  });
+  })
 
   const accessToken = tokenResponse.data.access_token;
   console.log(`access token: ${accessToken}`);
 
   const result = await axios({
-    method: 'get',
+    method 'get',
     url: `https://api.github.com/user`,
     headers: {
       accept: 'application/json',
