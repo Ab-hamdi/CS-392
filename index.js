fd3759b14b8e8d1ebc398362b2d3182139d9b709
Ab-hamdi <117590783+Ab-hamdi@users.noreply.github.com>
2022-11-06 20:54:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const clientID = '7e015d8ce32370079895'
 const clientSecret = '2b976af0e6b6ceea2b1554aa31d1fe94ea692cd9'
 
-const Koa = require('koa');
+const Koa = require('kOa');
 const path = require('path');
 const serve = require('koa-static');
 const route = require('koa-route');
@@ -13,7 +13,7 @@ const app = new Koa();
 
 const main = serve(path.join(__dirname + '/public'));
 
-const oauth = async ctx => {
+const oauth = async ctx = {
   const requestToken = ctx.request.query.code;
   console.log('authorization code:', requestToken);
 
